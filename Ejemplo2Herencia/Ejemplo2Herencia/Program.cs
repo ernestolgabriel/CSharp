57d581821cd6ff513f1dbf3f4a422f32b11e8a3f
gabrielernestol <gabrielernestol@gmail.com>
2021-11-15 15:53:01 -0600

    Inicio del Ejercicio 2 Herencia

---
+++ Ejemplo2Herencia/Ejemplo2Herencia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejemplo2Herencia
+{
+    class Producto
+    {
+        //atributos
+        private float precio;
+        private long codigo;
+        private string marca, nombre;
+        
+        //propiedades
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
